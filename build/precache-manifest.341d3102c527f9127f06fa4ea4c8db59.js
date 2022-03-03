e015a13de568bf4d090b803e5d8b242ae42a57f6
abinadi swapp <abinadiswapp@gmail.com>
2022-03-02 22:26:32 -0700

    added build so that it can be used to host on github

---
+++ build/precache-manifest.341d3102c527f9127f06fa4ea4c8db59.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "1965943c7e30ab6ceb89",
+    "url": "./static/css/main.aef2e1bc.chunk.css"
+  },
+  {
+    "revision": "1965943c7e30ab6ceb89",
+    "url": "./static/js/main.443130e0.chunk.js"
+  },
+  {
+    "revision": "3b078c8866268b1f5d57",
+    "url": "./static/js/runtime~main.17015936.js"
+  },
+  {
+    "revision": "98012c8e21f294e52ef8",
+    "url": "./static/js/2.fa3f2d11.chunk.js"
+  },
+  {
+    "revision": "f019a8d8b6b121e06049",
+    "url": "./static/js/3.ea86ce1b.chunk.js"
+  },
+  {
+    "revision": "5503ecc845dcff63f26e67d39392062f",
+    "url": "./static/media/yellow.5503ecc8.png"
+  },
+  {
+    "revision": "46b65a544ed9917116789284edf3634f",
+    "url": "./static/media/black.46b65a54.png"
+  },
+  {
+    "revision": "d065925dee233ee632571914910e65b5",
+    "url": "./static/media/white.d065925d.png"
+  },
+  {
+    "revision": "f867747b742dcab9d27f9dd0aade354e",
+    "url": "./static/media/green.f867747b.png"
+  },
+  {
+    "revision": "3b3c8ee7048cc67517efd6429575e5d5",
+    "url": "./static/media/orange.3b3c8ee7.png"
+  },
+  {
+    "revision": "d8391359b6baba1c010b24e23c1e0b74",
+    "url": "./static/media/blue.d8391359.png"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "./static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "89f28ba504e1537495ec59b20c64309c",
+    "url": "./static/media/red.89f28ba5.png"
+  },
+  {
+    "revision": "0dd6b5f4e7ed63c4e35eee987301925a",
+    "url": "./index.html"
+  }
+];
