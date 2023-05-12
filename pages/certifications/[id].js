a37a609742d5e1d456cb66f4422c9b111af11177
abinadi swapp <abinadiswapp@gmail.com>
2023-05-11 18:23:32 -0600

    fixed formating on projects and certifications

--- pages/certifications/[id].js
+++ pages/certifications/[id].js
@@ -25,13 +25,13 @@ export default function Certification({certData})
 {
     return(
         <div className=" w-full max-w-xl h-fit bg-white bg-opacity-90 text-black rounded-sm shrink-0">
-            <div className=" w-full p-3 h-28 border-black border-b-2 flex justify-between gap-3">
+            <div className=" w-full p-3 min-h-[112px] border-black border-b-2 flex justify-between gap-3">
                 <div className="flex flex-col justify-around">
-                    <div className="text-xl text-blue-700 font-mono">{certData.title}</div>
+                    <div className=" text-sm sm:text-base md:text-xl text-blue-700 font-mono">{certData.title}</div>
                     <div>{certData.date}</div>
                 </div>
-                <div className="h-full flex flex-col justify-start">
-                    {certData.link ? <a className="w-fit p-2 text-sm rounded-sm bg-slate-400 hover:bg-slate-500 overflow-hidden whitespace-nowrap" href={certData.link}>Certification Proof</a> : ""}
+                <div className="h-full flex flex-col min-w-[120px] justify-start">
+                    {certData.link ? <a className="w-fit p-2 text-xs md:text-sm rounded-sm bg-slate-400 hover:bg-slate-500 overflow-hidden whitespace-nowrap" href={certData.link}>Certification Proof</a> : ""}
                 </div>
             </div>
             <div className="p-3 flex flex-col">
